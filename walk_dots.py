928bc9976b794e3edab553003bd549603cbdf72c
Mya Schroder <Mya.Schroder@conocophillips.com>
2024-06-25 09:01:48 -0800

    Fixed appendages walking

--- walk_dots.py
+++ walk_dots.py
@@ -13,6 +13,7 @@ class semicircle:
                  x_step: float = 0.01,
                  r: float = 1, # Radius of semicircle
                  a: float = 0, # X position of center of circle
+                 a_step: float = 0.01, 
                  b: float = 0, # X position of center of circle
                  ):
 
@@ -22,6 +23,7 @@ class semicircle:
         self.x_step = x_step
         self.r = r
         self.a = a
+        self.a_step = a_step
         self.b = b
 
         self.y = 0
@@ -32,24 +34,24 @@ class semicircle:
         """
         Calculates y and then increments x by x_step. Bound between x_min and x_max.
         """
-
         self.y = -math.sqrt(self.r ** 2 - (self.x - self.a) ** 2) + self.b
-
-        self.x += self.x_step
-        if self.x > self.x_max or self.x < self.x_min:
+        
+        self.a += self.a_step
+        self.x += self.x_step + self.a_step
+        
+        if self.x > self.x_max + self.a or self.x  < self.x_min + self.a:
             self.x_step *= -1
-            self.x += self.x_step
-
+            self.x += self.x_step + self.a_step* self.x_step / abs(self.x_step) # Convert a_step to the same sign as x_step
 
 pos = math.cos(math.radians(65))  # To make 180-65-65 = 50 deg total stride
 appendages = [
     # Connected to lower torso (0,0)
-    {'plot': None, 'data': semicircle(-2*pos, -2*pos, 2*pos, 0.04, 2, 0, 0), 'connection': (0,0)},  # Leg 1 
-    {'plot': None, 'data': semicircle(2*pos, -2*pos, 2*pos, 0.04, 2, 0, 0), 'connection': (0,0)},  # Leg 2
-
+    {'plot': None, 'data': semicircle(-2*pos, -2*pos, 2*pos, 0.04, 2, 0, 0.01, 0)},  # Leg 1 
+    {'plot': None, 'data': semicircle(2*pos, -2*pos, 2*pos, 0.04, 2, 0, 0.01, 0)},  # Leg 2
+    
     # Connected to upper torso (0, 2) 
-    {'plot': None, 'data': semicircle(-1*pos, -1*pos, 1*pos, 0.02, 1, 0, 2), 'connection': (0,2)},  # Arm 1
-    {'plot': None, 'data': semicircle(1*pos, -1*pos, 1*pos, 0.02, 1, 0, 2), 'connection': (0,2)},  # Arm 2
+    {'plot': None, 'data': semicircle(-1*pos, -1*pos, 1*pos, 0.02, 1, 0, 0.01, 2)},  # Arm 1
+    {'plot': None, 'data': semicircle(1*pos, -1*pos, 1*pos, 0.02, 1, 0, 0.01, 2)},  # Arm 2
 ]
 
 fig = plt.figure()
@@ -60,16 +62,17 @@ for appendage in appendages:
     appendage['plot'], = ax.plot([appendage['data'].x],[appendage['data'].y], 'r-')
 
 # Connect torso and head
-torso, = ax.plot([0,0],[0,3], 'r-')
-head, = ax.plot([0],[3], 'ro', markersize=20)
+# torso, = ax.plot([0,0],[0,3], 'r-')
+# head, = ax.plot([0],[3], 'ro', markersize=20)
 
 # animation function.  This is called sequentially
 def animate(i):
 
     for appendage in appendages:
         appendage['data'].move() # Move appendages
-        # Plot appendages and attach them to torso
-        appendage['plot'].set_data([appendage['data'].x, appendage['connection'][0]],[appendage['data'].y, appendage['connection'][1]]) 
+        # Plot appendages and attach them to torso]
+        print("append a", appendage['data'].a)
+        appendage['plot'].set_data([appendage['data'].x, appendage['data'].a],[appendage['data'].y, appendage['data'].b]) 
 
     return appendage['plot'],
 
